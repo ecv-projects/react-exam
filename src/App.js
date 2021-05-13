d941ef5d5f47642ad60f067dcca30185465e4efd
julieSalha <julie.salha@gmail.com>
2021-05-13 13:15:55 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Home from './pages/Home'
+import Form from './pages/Form';
+import Popup from './pages/Popup';
+import Articles from './pages/Articles';
+import Article from './pages/Article';
+import CreateArticle from './pages/CreateArticle';
+import './tailwind.output.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/form" component={Form} />
+      <Route path="/popup" component={Popup} />
+      <Route path="/create-article" component={CreateArticle} />
+      <Route path="/articles/:id" component={Article} />
+      <Route path="/articles" component={Articles} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </Router>
+);
 
 export default App;
